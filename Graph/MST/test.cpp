25e83f69e6b5d15312523712c0a2842ae37deb47
TomorrowIppo <bjk1624@gmail.com>
2025-06-04 16:32:32 +0900

    feat: kruskal 전용 test.cpp에 dir flag 기반 방향, 무방향 설정 추가 + kruskal 결과 출력시 불필요한 출력 구문 삭제

--- Graph/MST/test.cpp
+++ Graph/MST/test.cpp
@@ -6,27 +6,16 @@
 using namespace std;
 
 int main() {
-    AdjacencyList g;
+    AdjacencyMatrix g(5);    // 무방향 그래프
 
     // 단방향이 기본이므로 양방향 간선을 직접 넣어줌
     g.insertEdge(1, 2, 1);
-    g.insertEdge(2, 1, 1);
-
     g.insertEdge(1, 3, 2);
-    g.insertEdge(3, 1, 2);
-
     g.insertEdge(2, 4, 11);
-    g.insertEdge(4, 2, 11);
-
     g.insertEdge(3, 5, 5);
-    g.insertEdge(5, 3, 5);
-
     g.insertEdge(5, 1, 7);
-    g.insertEdge(1, 5, 7);
 
     g.printGraph();
-
-    std::cout << "\nKruskal MST Edges:\n";
     GraphSearch::kruskal(g);
 
     return 0;
